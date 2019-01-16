dc8b3f7374e96a46fded23531b97a7a1e9de6388
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2019-01-16 10:43:08 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 console.clear();
 
-const _ = require("lodash");
-let str = "12\n\r3456789123456789123456789";
-
-const c = str.split(/\n\r/);
-// const c = _.chunk(str, 6);
-
-console.log(c, str);
+console.log(123);
